8aed2f43e960bf35bee2d955e72382fb417128b8
Eliav Lavi <eliavlavi@gmail.com>
2023-03-04 11:02:46 +0200

    update config

--- src/Config.tsx
+++ src/Config.tsx
@@ -1,3 +1,12 @@
+import styled from "styled-components";
+
+export const PanelMargins = styled.div`
+  padding: 10px 20px;
+  display: flex;
+  width: 100%;
+`;
+
+
 export function Config() {
-  return <div>config.</div>
+  return <PanelMargins>Coming soon...</PanelMargins>
 }
